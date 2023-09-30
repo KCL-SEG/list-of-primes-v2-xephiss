219b7887cb2f9dd0fc3425eb19482e1ff35b4227
Alex Lam <alex.lam@kcl.ac.uk>
2023-09-30 15:12:57 +0100

    Add prime-testing algorithm to method
    Also added an extra method to allow for more efficient divisibility testing

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        return ValueError(f'Error: Input value: {number_of_primes} is less than 1.')
+    elif number_of_primes % int(number_of_primes) != 0:
+        return ValueError(f'Error: Input value: {number_of_primes} is not an integer')
+    else:
+        primeList = [2]
+        next = 3
+        while len(primeList) != number_of_primes:
+            addNumberToList(next, primeList)
+            next += 1
+        return primeList
+
+
+def addNumberToList(number, primeList):
+    for i in primeList:
+        if number % i == 0:
+            return False
+    primeList.append(number)
